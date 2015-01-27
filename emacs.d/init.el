1eacc39e8b0088623d78a8de7de4b5f890dd5571
Jamie Culpon <jculpon@gmail.com>
2015-01-27 07:09:38 -0800

    Theme switchup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -54,6 +54,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ ;;'(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
  '(inhibit-startup-screen t)
  '(tool-bar-mode nil))
 (custom-set-faces
