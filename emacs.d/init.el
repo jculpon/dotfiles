650c4381c02d269ba8793fd7afeb2e6b1e3f14c8
Jamie Culpon <jculpon@gmail.com>
2015-02-03 02:23:39 -0800

    Personal configuration update
    Primary change is switching from Inconsolata back to Anonymous Pro becuase
    Anonymous Pro seems to be much more widely deployed these days. Probably not
    a big deal on OS X, but I'd like to be able to share this with folks who
    are not on darwin-based *nx ports.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,11 +1,9 @@
 ;; Packaging config
 (require 'package)
+
 ;; Include MELPA for elisp packages, not just FSF-blessed ones
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
-;; Uncomment to include marmalade (disabled because it lags behind MELPA)
-;; (add-to-list 'package-archives
-;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; Initialize package so that we can configure stuff from installed packages
 (setq package-enable-at-startup nil)
@@ -18,9 +16,6 @@
 ;; Don't use tabs
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
-;; TODO consider if aliasing other tab vars to make this explicit makes sense
-;; (defvaralias 'c-basic-offset 'tab-width)
-;; (defvaralias 'cperl-indent-level 'tab-width)
 
 ;; Spellcheck with aspell instead of ispell
 (setq ispell-program-name "aspell")
@@ -29,6 +24,7 @@
 ;; Enable flyspell for text and org
 (dolist (hook '(text-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1))))
+
 ;; Disable flyspell for major modes: uncomment and add to hook list
 (dolist (hook '(change-log-mode-hook log-edit-mode-hook))
   (add-hook hook (lambda () (flyspell-mode -1))))
@@ -50,17 +46,9 @@
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;;'(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(custom-enabled-themes (quote (tsdh-dark)))
  '(inhibit-startup-screen t)
  '(tool-bar-mode nil))
+
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:weight normal :height 140 :width normal :family "Inconsolata")))))
+ '(default ((t (:weight normal :height 140 :width normal :family "Anonymous Pro")))))
