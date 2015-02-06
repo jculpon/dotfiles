bf9e6b1b7223724b126aabd45433146cddf1c763
Jamie Culpon <jculpon@gmail.com>
2015-02-06 14:41:57 -0800

    Package updates

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -46,9 +46,18 @@
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (tsdh-dark)))
  '(inhibit-startup-screen t)
  '(tool-bar-mode nil))
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:weight normal :height 140 :width normal :family "Anonymous Pro")))))
