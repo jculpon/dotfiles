de9ba1ee41bfec5a052b3cc8913103f5e0fe08e8
Jamie Culpon <jculpon@gmail.com>
2015-03-12 08:10:23 -0700

    Dotfiles updates to make my retina machine overheat less
    Minor change, primarily to themes in emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -50,10 +50,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (tsdh-dark)))
- '(inhibit-startup-screen t)
- '(tool-bar-mode nil))
+ '(inhibit-startup-screen t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
