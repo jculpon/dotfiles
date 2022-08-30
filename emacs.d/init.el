e16b3bac9fea9134d40d6eb937c0abf64cb2a08a
Jamie Culpon <peri@foxontheinter.net>
2022-08-30 11:30:52 -0500

    Update emacs.d from ghanima[EOM]

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -56,23 +56,20 @@
  '(column-number-mode t)
  '(cua-mode t nil (cua-base))
  '(current-language-environment "UTF-8")
- '(cursor-type (quote bar))
- '(custom-enabled-themes (quote (zenburn)))
+ '(cursor-type 'bar)
+ '(custom-enabled-themes '(zenburn))
  '(custom-safe-themes
-   (quote
-    ("a3e99dbdaa138996bb0c9c806bc3c3c6b4fd61d6973b946d750b555af8b7555b" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "df3e05e16180d77732ceab47a43f2fcdb099714c1c47e91e8089d2fcf5882ea3" default)))
+   '("a3e99dbdaa138996bb0c9c806bc3c3c6b4fd61d6973b946d750b555af8b7555b" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "df3e05e16180d77732ceab47a43f2fcdb099714c1c47e91e8089d2fcf5882ea3" default))
  '(fci-rule-character-color "#192028")
- '(frame-brackground-mode (quote dark))
+ '(frame-brackground-mode 'dark)
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   (quote
-    (zenburn-theme yasnippet yaml-mode wrap-region warm-night-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme toml-mode toml spacegray-theme sos solarized-theme sicp seti-theme rust-mode osx-pseudo-daemon osx-plist osx-org-clock-menubar osx-location osx-dictionary osx-clipboard osx-browse markdown-toc markdown-mode+ magit-svn magit-gh-pulls gruber-darker-theme font-utils font-lock-studio font-lock+ flyparens flymake-yaml flymake-rust flymake-ruby flymake-python-pyflakes flymake-lua flymake-json flymake-jslint flymake-google-cpplint flycheck-rust flycheck-package flycheck-clojure flycheck-cask exec-path-from-shell color-theme-solarized chinese-word-at-point)))
+   '(xml+ xml-format xml-quotes xml-rpc xmlgen xmlunicode xo xonsh-mode xpm xquery-mode xquery-tool xr xref xref-js2 xref-rst xresources-theme xterm-color xterm-keybinder xtest xwidgete xwidgets-reuse xwiki-mode yaml yaml-imenu yaml-pro yaml-tomato yang-mode yoshi-theme ztree zweilight-theme minibuffer-header org-contrib org-notify sakura-theme sideline-blame sideline-flymake magit-p4 magit-stgit zenburn-theme yasnippet yaml-mode wrap-region warm-night-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme toml-mode toml spacegray-theme sos solarized-theme sicp seti-theme rust-mode osx-pseudo-daemon osx-plist osx-org-clock-menubar osx-location osx-dictionary osx-clipboard osx-browse markdown-toc markdown-mode+ magit-svn magit-gh-pulls gruber-darker-theme font-utils font-lock-studio font-lock+ flyparens flymake-yaml flymake-rust flymake-ruby flymake-python-pyflakes flymake-lua flymake-json flymake-jslint flymake-google-cpplint flycheck-rust flycheck-package flycheck-clojure flycheck-cask exec-path-from-shell color-theme-solarized chinese-word-at-point))
  '(text-mode-hook
-   (quote
-    (turn-on-auto-fill
+   '(turn-on-auto-fill
      (lambda nil
        (flyspell-mode 1))
-     text-mode-hook-identify)))
+     text-mode-hook-identify))
  '(tool-bar-mode nil))
 
 (custom-set-faces
@@ -80,4 +77,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:weight normal :height 140 :width normal :family "Anonymous Pro")))))
+ )
+
+;; end last known good update [jlc/peri]
